83bbe9248f1e5c6209978d2439be3e9888d89cc9
Peter Wells <petethebeatwells@gmail.com>
2019-10-22 21:09:57 -0700

    add javascript buttons and jquery animations to consolidate links

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".butts").click(function(event) {
+    $(event.target).parent().children(".linkDump").slideToggle(100);
+  });
+
+
+
+});
+
+// $("li").click(function(event){
+//   $(event.target).parent().children('p').slideToggle();
+//   window.setTimeout(toggleClassFunction, 200, event);
+// });
